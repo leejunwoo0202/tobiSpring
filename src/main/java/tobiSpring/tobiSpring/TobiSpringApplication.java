70428d3ef32d4042722b2811cb1ddcfbc1fa748f
준우 이 <gs04043@naver.com>
2024-01-02 23:43:23 +0900

    Initial commit

---
+++ src/main/java/tobiSpring/tobiSpring/TobiSpringApplication.java
@@ -0,0 +1,13 @@
+package tobiSpring.tobiSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobiSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TobiSpringApplication.class, args);
+	}
+
+}
