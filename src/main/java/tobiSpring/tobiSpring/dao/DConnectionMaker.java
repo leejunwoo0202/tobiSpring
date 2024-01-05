2e9a2e31c88f83a77c5efbe298d0d31d82ae0d62
준우 이 <gs04043@naver.com>
2024-01-06 00:57:43 +0900

    DI와 IOC를 메서드 주입으로 변경

--- src/main/java/tobiSpring/tobiSpring/dao/DConnectionMaker.java
+++ src/main/java/tobiSpring/tobiSpring/dao/DConnectionMaker.java
@@ -1,2 +1,13 @@
-package tobiSpring.tobiSpring.dao;public class DConnectionMaker {
+package tobiSpring.tobiSpring.dao;
+
+public class DConnectionMaer implements ConnectionMaker{
+
+    // connection 메소드
+    public void connection()
+    {
+        System.out.println("dconnection메소드 오버라이딩");
+
+    }
+
+
 }
