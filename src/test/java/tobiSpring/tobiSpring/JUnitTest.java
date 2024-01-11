e38b1787a3f5aa2832887d76982919c0618ebddc
준우 이 <gs04043@naver.com>
2024-01-11 21:39:49 +0900

    Junit 테스트 오브젝트 테스트

--- src/test/java/tobiSpring/tobiSpring/JUnitTest.java
+++ src/test/java/tobiSpring/tobiSpring/JUnitTest.java
@@ -4,30 +4,53 @@ import org.assertj.core.api.Assertions;
 import org.assertj.core.api.Condition;
 import org.junit.jupiter.api.Test;
 
-public class JUnitTest extends Condition<JUnitTest> {
-    static JUnitTest testObject;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+public class JUnitTest extends Condition<Collection<? extends JUnitTest>> {
+
+    static Set<JUnitTest> testObjects = new HashSet<>();
 
     @Test
     public void test1() {
-        System.out.println("this = " + this);
-        System.out.println("testObject = " + testObject);
-        Assertions.assertThat(this).isNotSameAs(testObject);
+
+        Assertions.assertThat(testObjects).isNot(this);
+        testObjects.add(this);
+        
+        for(JUnitTest element : testObjects)
+        {
+            System.out.println("element = " + element);
+            System.out.println("테스트 하나 종료 ");
+        }
+
+
 
     }
 
     @Test
     public void test2() {
-        System.out.println("this = " + this);
-        System.out.println("testObject = " + testObject);
-        Assertions.assertThat(this).isNotSameAs(testObject);
+        Assertions.assertThat(testObjects).isNotIn(this);
+        testObjects.add(this);
+
+        for(JUnitTest element : testObjects)
+        {
+            System.out.println("element = " + element);
+            System.out.println("테스트 하나 종료 ");
+        }
 
     }
 
     @Test
     public void test3() {
-        System.out.println("this = " + this);
-        System.out.println("testObject = " + testObject);
-        Assertions.assertThat(this).isNotSameAs(testObject);
-       
+        Assertions.assertThat(testObjects).isNotIn(this);
+        testObjects.add(this);
+
+        for(JUnitTest element : testObjects)
+        {
+            System.out.println("element = " + element);
+            System.out.println("테스트 하나 종료 ");
+        }
+
     }
 }
