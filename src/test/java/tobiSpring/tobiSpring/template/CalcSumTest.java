2433281fd22834c536f0f33a94e01c41f8a9d9fd
준우 이 <gs04043@naver.com>
2024-01-18 23:45:53 +0900

    템플릿 콜백 중복제거

--- src/test/java/tobiSpring/tobiSpring/template/CalcSumTest.java
+++ src/test/java/tobiSpring/tobiSpring/template/CalcSumTest.java
@@ -1,6 +1,7 @@
 package tobiSpring.tobiSpring.template;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import tobiSpring.tobiSpring.template.Calculator;
 
@@ -8,13 +9,28 @@ import java.io.IOException;
 
 public class CalcSumTest {
 
+    Calculator calculator;
+    String numFilepath;
+
+
+
+    @BeforeEach
+    public void setUp(){
+        this.calculator = new Calculator();
+        this.numFilepath = "src/main/java/tobiSpring/tobiSpring/numbers.txt";
+    }
+
+    @Test
+    public void multiplyOfNumbers() throws IOException {
+        Assertions.assertThat(calculator.calcMultiply(numFilepath)).isEqualTo(24);
+    }
+
+
+
     @Test
     public void sumOfNumbers() throws IOException {
-        Calculator calculator = new Calculator();
-        String filePath = "src/main/java/tobiSpring/tobiSpring/numbers.txt";
-        int sum = calculator.calcSum(filePath);
 
-        Assertions.assertThat(sum).isEqualTo(10);
+        Assertions.assertThat(calculator.calcSum(numFilepath)).isEqualTo(10);
 
     }
 }
--- src/test/java/tobiSpring/tobiSpring/template/Calculator.java
+++ src/test/java/tobiSpring/tobiSpring/template/Calculator.java
@@ -1,5 +1,7 @@
 package tobiSpring.tobiSpring.template;
 
+import org.assertj.core.api.AbstractBigDecimalAssert;
+
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -8,27 +10,75 @@ import java.nio.Buffer;
 public class Calculator {
 
     public Integer calcSum(String filepath) throws IOException {
+        BufferedReaderCallback sumCallback =
+                new BufferedReaderCallback() {
+                    @Override
+                    public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+
+                            Integer sum = 0;
+                            String line = null;
+                            while ((line = br.readLine()) != null) {
+                                sum += Integer.valueOf(line);
+                            }
+                            return sum;
+
+                    }
+                };
+        return fileReadTemplate(filepath, sumCallback);
+
+    }
+
+    public Integer calcMultiply(String filepath) throws IOException {
+        BufferedReaderCallback multiplyCallback =
+                new BufferedReaderCallback() {
+                    @Override
+                    public Integer doSomethingWithReader(BufferedReader br) throws IOException {
+
+                        Integer multiply = 1;
+                        String line = null;
+                        while((line = br.readLine()) != null){
+                            multiply *= Integer.valueOf(line);
+                        }
+
+                        return multiply;
+
+
+                    }
+                };
+
+        return fileReadTemplate(filepath, multiplyCallback);
+    }
+
+
+
+    public Integer fileReadTemplate(String filepath, BufferedReaderCallback callback)
+            throws IOException {
+
         BufferedReader br = null;
 
         try {
             br = new BufferedReader(new FileReader(filepath));
-            Integer sum = 0;
-            String line = null;
-            while ((line = br.readLine()) != null) {
-                sum += Integer.valueOf(line);
-            }
-            return sum;
-        } catch (IOException e) {
+            int ret = callback.doSomethingWithReader(br);
+            return ret;
+        }
+        catch (IOException e)
+        {
             System.out.println(e.getMessage());
             throw e;
-        } finally {
-            if (br != null) {
+        }
+        finally
+        {
+            if (br != null)
+            {
                 try {
                     br.close();
                 } catch (IOException e) {
                     System.out.println(e.getMessage());
                 }
             }
+
         }
     }
+
+
 }
